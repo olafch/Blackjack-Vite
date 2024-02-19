752bf24ce6381fae09b4e096ac9901971c3ea640
Olaf Chwolka <olaf.chwolka@outlook.com>
2024-02-19 16:54:51 +0100

    Add JSDocs to create-deck function

--- src/blackjack/usecases/create-deck.js
+++ src/blackjack/usecases/create-deck.js
@@ -1,7 +1,20 @@
 import _ from "underscore";
 
+/**
+ * Esta función crea un nuevo deck
+ * @param {Array<String>} cartTypes Ejemplo: ["C", "D", "H", "S"]
+ * @param {Array<String>} specialTypes Ejemplo: ["A", "J", "Q", "K"]
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
+
 export const crearDeck = (cartTypes, specialTypes) => {
   let deck = [];
+
+  if (!cartTypes || cartTypes.length === 0)
+    throw new Error("cartTypes is compulsory as a string array");
+
+  if (!specialTypes || specialTypes.length === 0)
+    throw new Error("specialTypes is compulsory as a string array");
 
   for (let i = 2; i <= 10; i++) {
     for (let tipo of cartTypes) {
